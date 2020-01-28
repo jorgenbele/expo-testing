6b80c1cafbefeea60b1fef17317dbf4d5db040e7
Jørgen Bele Reinfjell <jorgen.bele@gmail.com>
2020-01-29 00:13:56 +0100

    working display of workspaces

--- src/screens/WorkspacesScreen.js
+++ src/screens/WorkspacesScreen.js
@@ -3,12 +3,12 @@ import { StyleSheet, Text, View } from "react-native";
 
 import PopupInfoBanner from "../components/PopupInfoBanner";
 
-import { Avatar } from 'react-native-paper'
+import { Avatar, List } from "react-native-paper";
 
 export default class WorkspacesScreen extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {workspaces: this.props.workspaces}
+    super(props);
+    this.state = { workspaces: this.props.workspaces };
   }
 
   render() {
@@ -21,16 +21,56 @@ export default class WorkspacesScreen extends React.Component {
       }
     });
 
+    const WORKSPACES = [
+      { name: "Kollektiv", isOwner: true },
+      { name: "Hjemme", isOwner: false }
+    ];
+
+    const isWorkspaceOwner = workspace => workspace.isOwner;
+
+    const managingWorkspaces = WORKSPACES.filter(isWorkspaceOwner);
+    const joinedWorkspaces = WORKSPACES.filter(w => !isWorkspaceOwner(w));
+
     //const iconicon = makeIcon(true, 'list');
     return (
       <>
-        {this.state.workspaces.length <= 0 &&
-          <PopupInfoBanner visible={true} 
-              message={'You are currently without a workspace'} 
-              confirmLabel={'Add a workspace'} 
-              confirmAction={() => {this.state.workspaces = ['test']}}
-              ignoreLabel={'Not now'}
-              icon="exclamation" />
+        {
+          //this.state.workspaces.length <= 0 ? (
+          // <PopupInfoBanner
+          //   visible={true}
+          //   message={"You are currently without a workspace"}
+          //   confirmLabel={"Add a workspace"}
+          //   confirmAction={() => {
+          //     this.state.workspaces.add(["test"]);
+          //   }}
+          //   ignoreLabel={"Not now"}
+          //   icon="exclamation"
+          // />
+          // ) : (
+          <List.Section>
+            {managingWorkspaces.length > 0 && (
+              <>
+                <List.Subheader>Workspaces you manage</List.Subheader>
+                {managingWorkspaces.map((workspace, index) => {
+                  return <List.Item
+                    key={workspace + index.toString()}
+                    title={'test'}
+                    left={() => <List.Icon icon="folder" />}
+                  />;
+                })}
+              </>
+            )}
+
+            <List.Subheader>Workspaces you are part of</List.Subheader>
+            {joinedWorkspaces.map((workspace, index) => {
+              return <List.Item
+                key={workspace + index.toString()}
+                title={workspace.name}
+                left={() => <List.Icon icon="folder" />}
+              />;
+            })}
+          </List.Section>
+          //)
         }
       </>
     );
@@ -38,5 +78,8 @@ export default class WorkspacesScreen extends React.Component {
 }
 
 WorkspacesScreen.defaultProps = {
-  workspaces: [],
-}
+  workspaces: [
+    { name: "Kollektiv", isOwner: true },
+    { name: "Hjemme", isOwner: false }
+  ]
+};
